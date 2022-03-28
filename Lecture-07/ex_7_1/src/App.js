c0aac9a4289f0b24f30b0f50d39feb47bded544d
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-03-28 16:10:59 +0300

    Statistics show when the all counter is above 0

--- Lecture-07/ex_7_1/src/App.js
+++ Lecture-07/ex_7_1/src/App.js
@@ -7,19 +7,30 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
 
+  if(all === 0){
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={() => setGood(good + 1) + setAll(all + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1) + setAll(all + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1) + setAll(all + 1)}>Bad</button>
+      </div>
+    )
+  }
+  
   return (
     <div>
-      <h1>give feedback</h1>
+      <h1>Give feedback</h1>
       <button onClick={() => setGood(good + 1) + setAll(all + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1) + setAll(all + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1) + setAll(all + 1)}>Bad</button>
-      <h1>statistics</h1>
+      <h1>Statistics</h1>
       Good: {good} <br/>
       Neutral: {neutral} <br/>
       Bad: {bad} <br/>
-      all: {all} <br/>
-      average: {((good-bad) / all).toFixed(2)} <br/>
-      positive: {((good/bad) * 100).toFixed(2)}% <br/>
+      All: {all} <br/>
+      Average: {((good-bad) / all).toFixed(2)} <br/>
+      Positive: {((good/all) * 100).toFixed(2)}% <br/>
     </div>
   )
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Full-Stack-Web-Development
-A course I am taking that teaches how to build and maintain a full stack web application.
+A course I am taking that teaches how to build and maintain a full stack web application. This course is based on the Full Stack Open self study course offered by the University of Helsinki. This version of the course is instead taught by a lecturer in Metropolia UAS.
 
 ## Lecture 01
 Was simply an introduction to the course and its contents.
@@ -11,9 +11,10 @@ Introduced the use of Emmet, CSS and Git.
 Javascript catchup.
 
 ## Lecture 04-05
-
+More javascript.
 
 ## Lecture 06
-
+First React app. Learned how to refactor components.
 
 ## Lecture 07
+Worked on a counter app.
