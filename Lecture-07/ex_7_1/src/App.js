69915c140df555bea349f511f941a5080815433e
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-03-28 20:37:29 +0300

    ex_7_1 changed again based on feedback, start of ex_7_2

--- Lecture-07/ex_7_1/src/App.js
+++ Lecture-07/ex_7_1/src/App.js
@@ -1,52 +1,71 @@
-import { useState } from 'react'
+import React, {useState} from 'react'
 
-const Header = ({name}) => <h1>{name}</h1>
-
-const Button = ({onClick, text}) => (
-  <button onClick={onClick}>{text}</button>
-)
-
-const Statistics = (props) => {
-  console.log(props);
+const Button = ({title, button_click}) => {
   return (
-    <>
-    <h1>Statistics</h1>
-    Good: {props.good} <br/>
-    Neutral: {props.neutral} <br/>
-    Bad: {props.bad} <br/>
-    All: {props.all} <br/>
-    Average: {((props.good-props.bad) / props.all).toFixed(1)} <br/>
-    Positive: {((props.good/props.all) * 100).toFixed(1)}% <br/>
-    </>
-  )}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-
-  if(all === 0){
-    return (
-      <div>
-        <Header name = "Give feedback"/>
-        <Button onClick={() => setGood(good + 1) + setAll(all + 1)} text='Good' />
-        <Button onClick={() => setNeutral(neutral + 1) + setAll(all + 1)} text='Neutral'/>
-        <Button onClick={() => setBad(bad + 1) + setAll(all + 1)} text='Bad'/>
-      </div>
-    )
-  }
-  
-  return (
-    <div>
-      <Header name = "Give feedback"/>
-      <Button onClick={() => setGood(good + 1) + setAll(all + 1)} text='Good' />
-      <Button onClick={() => setNeutral(neutral + 1) + setAll(all + 1)} text='Neutral'/>
-      <Button onClick={() => setBad(bad + 1) + setAll(all + 1)} text='Bad'/>
-      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
-    </div>
+    <button onClick={button_click}>{title}</button>
   )
 }
 
-export default App
+const Statistics= ({title, value}) => {
+  if(title==="positive") {
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{value}%</td>
+    </tr>
+  )
+}
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const App = () => {
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+  const [all,setAll] = useState(0)
+
+  const increment_good = () => {setGood(good + 1); setAll(all + 1);}
+  const increment_neutral = () => {setNeutral(neutral + 1); setAll(all + 1);}
+  const increment_bad = () => {setBad(bad + 1); setAll(all + 1);}
+
+  if(all===0)
+  {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button title="good" button_click={increment_good} />
+        <Button title="neutral" button_click={increment_neutral} />
+        <Button title="bad" button_click={increment_bad} />
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button title="good" button_click={increment_good} />
+      <Button title="neutral" button_click={increment_neutral} />
+      <Button title="bad" button_click={increment_bad} />
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <Statistics title="good" value={good}/>
+          <Statistics title="neutral" value={neutral} />
+          <Statistics title="bad" value={bad} />
+          <Statistics title="all" value={all} />
+          <Statistics title="average" value={(good - bad) / all} />
+          <Statistics title="positive" value={(good / all ) * 100} />
+        </tbody>
+      </table>
+    </div>
+  );
+
+}
+
+export default App;
